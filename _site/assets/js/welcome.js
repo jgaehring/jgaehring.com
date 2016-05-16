5a1ae2fa111c242b0889306bca7a129833ff1764
Jamie <gaehj457@gmail.com>
2016-05-16 16:17:50 -0400

    hide/show text of the intro card with jquery, also making my sass a little classier

---
+++ _site/assets/js/welcome.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.intro-card').mouseenter(function () {
+        $('.card-back').css('display', 'inline-block');
+      });
+
+    $('.intro-card').mouseleave(function () {
+        $('.card-back').css('display', 'none');
+    });
+
+  });
