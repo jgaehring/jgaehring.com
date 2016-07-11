195a264384098ba3805869ffa10e67910d53a9e6
Jamie <gaehj457@gmail.com>
2016-07-11 17:33:20 -0400

    opening pageload animation

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,5 +1,7 @@
 $(function () {
 
+    $('.card-front > img').delay(1000).fadeOut(3000);
+
     $('.intro-card').mouseenter(function () {
         $('.card-back').css('display', 'inline-block');
       });
