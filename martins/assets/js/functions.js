d8e24d6ded29113fdb16e7b22f7e3c5695910f2e
James Gaehring <gaehj457@gmail.com>
2016-10-19 18:42:27 -0400

    adding martins preview 2

--- martins/assets/js/functions.js
+++ martins/assets/js/functions.js
@@ -1,22 +1,58 @@
 $(function() {
 
+// Toggling Main Nav
+
   $('.nav-toggle').on('click', function() {
-    var status = $(this).hasClass('is-open');
-    if(status) {
-      $('header, .nav-toggle, .nav-open, .nav-close').removeClass('is-open');
-    }
-    else {
-      $('header, .nav-toggle, .nav-open, .nav-close').addClass('is-open');
-    }
+    $('header, .nav-toggle, .nav-open, .nav-close').toggleClass('is-open');
   })
 
-  // $('.nav-toggle').on('click', function() {
-  //   $('header').toggleClass('is-open');
-  //   return false;
+// Activating Modals
+// TODO: Make this into a reusable function
+
+  $('#wholesale-inquiry').on('click', function() {
+    $('#wholesale-modal').addClass('is-opening');
+    setTimeout(function() {
+      $('#wholesale-modal').addClass('is-open');
+      $('#wholesale-modal').removeClass('is-opening');
+    }, 300);
+  })
+
+  $('#contact-btn').on('click', function() {
+    $('#contact-modal').addClass('is-opening');
+    setTimeout(function() {
+      $('#contact-modal').addClass('is-open');
+      $('#contact-modal').removeClass('is-opening');
+    }, 300);
+  })
+
+
+  $('#wholesale-modal .close-modal').on('click', function() {
+    $('#wholesale-modal').addClass('is-closing');
+    $('#wholesale-modal').removeClass('is-open');
+    setTimeout(function() {
+      $('#wholesale-modal').removeClass('is-closing');
+    }, 300);
+  })
+
+  $('#contact-modal .close-modal').on('click', function() {
+    $('#contact-modal').addClass('is-closing');
+    $('#contact-modal').removeClass('is-open');
+    setTimeout(function() {
+      $('#contact-modal').removeClass('is-closing');
+    }, 300);
+  })
+
+  // ORIGINAL FUNCTION
+  // $('.close-modal').on('click', function() {
+  //   $('.modal').addClass('is-closing');
+  //   $('.modal').removeClass('is-open');
+  //   setTimeout(function() {
+  //     $('.modal').removeClass('is-closing');
+  //   }, 300);
   // })
-  //
-  // $('header').hasClass('is-open', function() {
-  //   $('svg.nav-open')
-  // })
+
+
+// TODO: Allow modal to be closed by clicking outside modal-content
+
 
 });
