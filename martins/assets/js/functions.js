cdd589a17b9c7318bd0ba5dbe630f0caf159226e
James Gaehring <gaehj457@gmail.com>
2016-12-15 22:08:06 -0500

    adding martins preview 3

--- martins/assets/js/functions.js
+++ martins/assets/js/functions.js
@@ -1,58 +1,104 @@
 $(function() {
 
-// Toggling Main Nav
+  // Toggling Main Nav
 
   $('.nav-toggle').on('click', function() {
     $('header, .nav-toggle, .nav-open, .nav-close').toggleClass('is-open');
   })
 
-// Activating Modals
-// TODO: Make this into a reusable function
+  // MODALS
 
-  $('#wholesale-inquiry').on('click', function() {
-    $('#wholesale-modal').addClass('is-opening');
+  // Opening Modals
+
+  const openModal = function(modalOpener, modalContent) {
+    modalOpener.on('click', function() {
+      modalContent.addClass('is-opening');
+      setTimeout(function() {
+        modalContent.addClass('is-open');
+        modalContent.removeClass('is-opening');
+      }, 300);
+    })
+  }
+
+  openModal( $('#login-link'), $('#login-modal') );
+  openModal( $('#contact-btn'), $('#contact-modal') );
+  openModal( $('#wholesale-inquiry'), $('#wholesale-modal') );
+  openModal( $('#register-link'), $('#register-modal') );
+  openModal( $('#login-from-register'), $('#login-modal') );
+
+
+  // Closing Modals
+
+  const closeModal = function(modalCloser, modalContent) {
+    modalCloser.on('click', function() {
+      modalContent.addClass('is-closing');
+      modalContent.removeClass('is-open');
+      setTimeout(function() {
+        modalContent.removeClass('is-closing');
+      }, 300);
+    })
+  }
+
+  closeModal( $('#login-modal .close-modal'), $('#login-modal') );
+  closeModal( $('#contact-modal .close-modal'), $('#contact-modal') );
+  closeModal( $('#wholesale-modal .close-modal'), $('#wholesale-modal') );
+  closeModal( $('#register-modal .close-modal'), $('#register-modal') );
+  closeModal( $('#register-link'), $('#login-modal') );
+  closeModal( $('#login-from-register'), $('#register-modal'))
+
+
+  // TODO: Allow modal to be closed by clicking outside modal-content
+
+  // Thank you after registering user
+
+  $('#register-modal .btn').on('click', function() {
+    $('#register-modal form').hide();
+    $('#thank-you').show();
     setTimeout(function() {
-      $('#wholesale-modal').addClass('is-open');
-      $('#wholesale-modal').removeClass('is-opening');
-    }, 300);
+      $('#register-modal').addClass('is-closing');
+      $('#register-modal').removeClass('is-open');
+      setTimeout(function() {
+        $('#register-modal').removeClass('is-closing');
+      }, 300);
+    }, 2500)
   })
 
-  $('#contact-btn').on('click', function() {
-    $('#contact-modal').addClass('is-opening');
-    setTimeout(function() {
-      $('#contact-modal').addClass('is-open');
-      $('#contact-modal').removeClass('is-opening');
-    }, 300);
+
+// jQueryUI
+
+  $('#billing-state').selectmenu({
+    width: false
+  });
+
+  $('#billing-state').menu()
+
+  $('#shipping-is-billing').checkboxradio()
+
+  $('#shipping-state').selectmenu({
+    width: false
+  });
+  $('#shipping-state').menu();
+
+  $('#shipping-method').selectmenu({
+    width: false
+  });
+  $('#shipping-method').menu();
+
+  $('#card-type').selectmenu({
+    width: false
+  });
+  $('#card-type').menu();
+
+  $('#expiry-month').selectmenu({
+    width: false
   })
+  $('#expiry-month').menu()
 
 
-  $('#wholesale-modal .close-modal').on('click', function() {
-    $('#wholesale-modal').addClass('is-closing');
-    $('#wholesale-modal').removeClass('is-open');
-    setTimeout(function() {
-      $('#wholesale-modal').removeClass('is-closing');
-    }, 300);
+  $('#expiry-year').selectmenu({
+    width: false
   })
-
-  $('#contact-modal .close-modal').on('click', function() {
-    $('#contact-modal').addClass('is-closing');
-    $('#contact-modal').removeClass('is-open');
-    setTimeout(function() {
-      $('#contact-modal').removeClass('is-closing');
-    }, 300);
-  })
-
-  // ORIGINAL FUNCTION
-  // $('.close-modal').on('click', function() {
-  //   $('.modal').addClass('is-closing');
-  //   $('.modal').removeClass('is-open');
-  //   setTimeout(function() {
-  //     $('.modal').removeClass('is-closing');
-  //   }, 300);
-  // })
-
-
-// TODO: Allow modal to be closed by clicking outside modal-content
+  $('#expiry-year').menu()
 
 
 });
