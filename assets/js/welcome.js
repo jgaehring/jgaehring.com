cbaa450caada5d4606a01ce0addb73659919a1ab
James Gaehring <gaehj457@gmail.com>
2017-01-17 19:17:32 -0500

    tweaking card fade

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,9 +1,12 @@
 $(function () {
 
-    $('.card-front > img').delay(1000).fadeOut(3000);
+    $('.card-front img').delay(750).fadeOut(1500);
 
     $('.intro-card').mouseenter(function () {
+      if ($('.card-front img').css('display') == 'none') {
         $('.card-back').css('display', 'inline-block');
+
+      }
       });
 
     $('.intro-card').mouseleave(function () {
