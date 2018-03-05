9a949d438cd363be6a180f41e62d81340ef1f6b4
Jamie Gaehring <gaehj457@gmail.com>
2018-03-05 16:30:27 -0500

    removing lincoln theme

--- new-site/src/utils/typography.js
+++ new-site/src/utils/typography.js
@@ -1,20 +1,23 @@
 import Typography from 'typography';
-import theme from 'typography-theme-lincoln';
 
-theme.googleFonts = [
-  {
-    name: 'PT Serif',
-    styles: ['400', '400i', '700', '700i']
-  },
-  {
-    name: 'PT Mono',
-    styles: ['400'],
-  }
-];
-theme.headerFontFamily = ['PT Serif', 'serif'];
-theme.headerWeight = '700';
-theme.bodyFontFamily = ['PT Mono', 'monospace'];
-
-const typography = new Typography(theme);
+const typography = new Typography({
+  // Base font size & lineheight borrowed from Lincoln theme:
+  // https://github.com/KyleAMathews/typography.js/blob/master/packages/typography-theme-lincoln/src/index.js
+  baseFontSize: '19px',
+  baseLineHeight: 1.58,
+  googleFonts: [
+    {
+      name: 'PT Serif',
+      styles: ['400', '400i', '700', '700i']
+    },
+    {
+      name: 'PT Mono',
+      styles: ['400'],
+    }
+  ],
+  headerFontFamily: ['PT Serif', 'serif'],
+  headerWeight: '700',
+  bodyFontFamily: ['PT Mono', 'monospace']
+});
 
 export default typography;
