6a6193634c6b01e14becf9139d2b76b5c3610b89
Jamie Gaehring <gaehj457@gmail.com>
2018-04-23 16:18:31 -0400

    display blog posts on home page (needs work)

--- new-site/src/pages/index.js
+++ new-site/src/pages/index.js
@@ -5,7 +5,7 @@ import logo from '../assets/golden-russet-black.svg'
 import portrait from '../assets/portrait.jpg'
 import { FaGithub } from 'react-icons/lib/fa'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div className={styles.home}>
     <section className={styles.about}>
       <div className={styles.logo} >
@@ -28,11 +28,51 @@ const IndexPage = () => (
     <section className={styles.blog}>
       <header>
         <h2>Writings</h2>
-        <p>These are some things I've written.</p>
-        <p><Link to='/blog'>Go to blog.</Link></p>
+        <p>These are some things I've written. <Link to='/blog'>Go to blog.</Link></p>
       </header>
+      {
+        data.allMarkdownRemark.edges.map( ({node}) => 
+          <div key={node.id} className={styles.post}>
+            <Link to={`/${node.fields.slug}`}>
+              <h2>{node.frontmatter.title}</h2>
+            </Link>
+            <h6>{node.frontmatter.date}</h6>
+            <p>{node.excerpt}</p>
+          </div>
+        )
+      }
     </section>
   </div>
 )
 
 export default IndexPage
+
+/**
+  * TODO: Figure out how to put this into a reuseable component,
+  * which could be shared with `blog.js`. I haven't even decided
+  * how I want to filter and sort these yet (perhaps as a "highlights"
+  * reel?), but it would be nice to just pass props the reusable component 
+  * to handle that. Or perhaps use a higher order component somehow?
+  * Maybe extract the query itself to a composable function, so I 
+  * don't have to keep reusing all this query boilerplate.
+**/
+export const query = graphql`
+  query BlogList {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC } ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug 
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
