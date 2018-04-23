c4f9bb22709583be86b8297b6a8cb343fa48d44f
Jamie Gaehring <gaehj457@gmail.com>
2018-04-22 20:15:11 -0400

    testing out the `static` folder for portfolio pages

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
+    'gatsby-plugin-remove-trailing-slashes',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
