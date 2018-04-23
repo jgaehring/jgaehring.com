2d460880c4c8e43635f817ab6d1fed51fc7e19b1
Jamie Gaehring <gaehj457@gmail.com>
2018-04-22 21:37:39 -0400

    tweaking the typography a bit

--- new-site/src/utils/typography.js
+++ new-site/src/utils/typography.js
@@ -3,22 +3,23 @@ import Typography from 'typography';
 const typography = new Typography({
   // Base font size & lineheight borrowed from Lincoln theme:
   // https://github.com/KyleAMathews/typography.js/blob/master/packages/typography-theme-lincoln/src/index.js
-  baseFontSize: '19px',
-  baseLineHeight: 1.58,
+  baseFontSize: '16px',
+  baseLineHeight: 1.5,
+  scaleRatio: 3.81,
   googleFonts: [
-    {
-      name: 'PT Serif',
-      styles: ['400', '400i', '700', '700i']
-    },
     {
       name: 'PT Mono',
       styles: ['400'],
-    }
+    },
+    {
+      name: 'Montserrat',
+      styles: ['400', '400i', '700', '700i']
+    },
   ],
-  headerFontFamily: ['PT Serif', 'serif'],
-  headerWeight: '700',
-  bodyFontFamily: ['PT Mono', 'monospace'],
-  includeNormalize: true
+  includeNormalize: true,
+  headerWeight: '400',
+  headerFontFamily: ['PT Mono', 'monospace'],
+  bodyFontFamily: ['Montserrat', 'sans serif'],
 });
 
 export default typography;
