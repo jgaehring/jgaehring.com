7a1f43e22ede8cf9bd61eb7ee0fc8e7562777ad6
Jamie Gaehring <gaehj457@gmail.com>
2018-04-23 14:00:21 -0400

    adding contact info/links/icons

--- new-site/src/pages/index.js
+++ new-site/src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import styles from './index.module.css'
 import logo from '../assets/golden-russet-black.svg'
 import portrait from '../assets/portrait.jpg'
+import { FaGithub } from 'react-icons/lib/fa'
 
 const IndexPage = () => (
   <div className={styles.home}>
@@ -16,6 +17,12 @@ const IndexPage = () => (
       </div>
       <div className={styles.blurb} >
         <p>For nearly two decades, I've been helping farmers build a more open food system. Today, I'm working to give those farmers better access to the software, data and other tools that they need to feed their communities in the 21<sup>st</sup> century.</p>
+        <h3 id={styles.contact}>
+          &#123;{/*Use HTML code for curly braces b/c of jsx*/}
+          <a href="mailto:jamie@jgaehring.com" target='_blank'>jamie@jgaehring.com</a>
+          &#125;&nbsp;
+          <a href="https://github.com/jgaehring" target='_blank'><FaGithub/></a>
+        </h3>
       </div>
     </section>
     <section>
