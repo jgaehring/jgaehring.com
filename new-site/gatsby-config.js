b4333383ad12b8f448cc9191997142fe4d76e99c
Jamie Gaehring <gaehj457@gmail.com>
2018-05-01 14:33:28 -0400

    updating metadata

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Jamie Gaehring',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- new-site/src/layouts/index.js
+++ new-site/src/layouts/index.js
@@ -9,8 +9,8 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="Jamie Gaehring"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: "For nearly two decades, I've been helping farmers build a more open food system. Today, I'm working to give those farmers better access to the software, data and other tools that they need to feed their communities in the 21st century." },
+        { name: 'keywords', content: 'agriculture, technology, software, data, open source, farming, hudson valley, new york city, new jersey, greenmarket, javascript, web development, local food' },
       ]}
     >
       <link rel='shortcut icon' href={favicon} />
