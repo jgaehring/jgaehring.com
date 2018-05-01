467bbed2901603495d38cf1893fdaee42a9969c6
Jamie Gaehring <gaehj457@gmail.com>
2018-05-01 13:33:38 -0400

    feat: adding favicon

--- new-site/src/layouts/index.js
+++ new-site/src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import './index.css'
+import favicon from '../assets/favicon.ico'
 
 const TemplateWrapper = ({ children }) => (
   <div id='site-container'>
@@ -11,7 +12,9 @@ const TemplateWrapper = ({ children }) => (
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
-    />
+    >
+      <link rel='shortcut icon' href={favicon} />
+    </Helmet>
     {children()}
   </div>
 )
