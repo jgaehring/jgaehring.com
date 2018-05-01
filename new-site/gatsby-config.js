21e284c5945b3542bd9e0f1e1ced58358515ace6
Jamie Gaehring <gaehj457@gmail.com>
2018-05-01 13:30:47 -0400

    feat: adding manifest and offline plugins

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -7,6 +7,19 @@ module.exports = {
     'gatsby-plugin-catch-links',
     'gatsby-plugin-remove-trailing-slashes',
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'jgaehring.com',
+        short_name: 'jgaehring',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#ffffff',
+        display: 'minimal-ui',
+        icon: 'src/assets/favicon.png'
+      }
+    },
+    'gatsby-plugin-offline',
+    {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js',
