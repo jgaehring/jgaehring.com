29bc5deff95ff50510c0053f2350267d56e9dcbc
Jamie Gaehring <gaehj457@gmail.com>
2018-10-07 13:44:46 -0400

    replace boundActionCreators (v.1) with actions (v.2) for upgrade

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,8 +6,8 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  const { createNodeField } = boundActionCreators;
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
   if (node.internal.type === 'MarkdownRemark') {
     const fileNode = getNode(node.parent);
     const slug = createFilePath({ 
@@ -24,8 +24,8 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 };
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
   
   return new Promise( (resolve, reject) => {
     graphql(`
