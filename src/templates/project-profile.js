43be5cb5a0fd35d3e3a2c787bda175604ffd20fa
Jamie Gaehring <gaehj457@gmail.com>
2018-10-07 19:05:30 -0400

    markup & style the screenshot in project profile

--- src/templates/project-profile.js
+++ src/templates/project-profile.js
@@ -2,25 +2,31 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
-import styles from './blog-post.module.css';
+import styles from './project-profile.module.css';
 
 export default function BlogTemplate({ data }) {
   const { markdownRemark: post } = data;
   const rootUrl = 'https://jgaehring.com/'
   return (
     <Layout>
+      <Helmet
+        title={`${post.frontmatter.title} | Jamie Gaehring`}
+        meta={[
+          { name: 'description', content: post.excerpt },
+        ]}
+        link={[
+          { rel: 'canonical', href: rootUrl + data.markdownRemark.fields.slug }
+        ]}
+      >
+      </Helmet>
+      <Header/>
+      <div className={styles.screenshot}>
+        <img
+          alt='Screenshot of the Harvest Calendar Generator' 
+          src={post.frontmatter.cover.publicURL}
+        />
+      </div>
       <div className={styles.post}>
-        <Helmet
-          title={`${post.frontmatter.title} | Jamie Gaehring`}
-          meta={[
-            { name: 'description', content: post.excerpt },
-          ]}
-          link={[
-            { rel: 'canonical', href: rootUrl + data.markdownRemark.fields.slug }
-          ]}
-        >
-        </Helmet>
-        <Header/>
         <h1 className={styles.title}>{post.frontmatter.title}</h1>
         <div
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -39,7 +45,7 @@ export const pageQuery = graphql`
         path
         title
         cover {
-          relativePath
+          publicURL
         }
         thumb
       }
