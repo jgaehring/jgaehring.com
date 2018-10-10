67d1b638170504d0d99784d30cd0a5a7e6da60a6
Jamie Gaehring <gaehj457@gmail.com>
2018-10-10 00:18:23 -0400

    generate a basic preview of each project on the /projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <Header/>
     <h1>Projects</h1>
@@ -15,5 +16,55 @@ export default () => (
       deserunt exercitation, ubi quid veniam labore nescius ita noster cernantur ita 
       illum dolor.
     </p>
+    {
+      data.allMarkdownRemark.edges
+      .map(({ node: { frontmatter, fields: { slug }} }, i) => (
+        <div className='project' key={`project-${i}`}>
+          <Link to={`/${slug}`}>
+            <h3>{frontmatter.title}</h3>
+          </Link>
+          <Link to={`/${slug}`}>
+            <img 
+              src={frontmatter.cover.publicURL}
+              alt={`Screenshot of ${frontmatter.title}`}
+            />
+          </Link>
+          <p>{frontmatter.description}</p>
+        </div>
+      ))
+    }
   </Layout>
 )
+
+export const query = graphql`
+  query ProjectsQuery {
+    allMarkdownRemark( 
+      filter: { collection: { eq: "projects" } }
+      sort: { fields: [frontmatter___date], order: DESC } 
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            cover {
+              publicURL
+            }
+            thumb
+            github
+            link
+            description
+            stack
+          }
+          fields {
+            slug 
+          }
+          excerpt
+          collection
+        }
+      }
+    }
+  }
+`;
