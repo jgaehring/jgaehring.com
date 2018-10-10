2445a4aec15762edca175d6bd2be4854bea06ef9
Jamie Gaehring <gaehj457@gmail.com>
2018-10-10 02:32:06 -0400

    delete lorem from /projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,15 +7,6 @@ export default ({ data }) => (
   <Layout>
     <Header/>
     <h1>Projects</h1>
-    <p>
-      Tamen iis do velit ingeniis. Cupidatat illum nescius, qui quamquam praesentibus. 
-      Eu nisi enim aliqua possumus, anim ubi senserit an labore.Ex dolor mandaremus 
-      excepteur. Qui export incididunt adipisicing est se anim reprehenderit. Quid 
-      ingeniis ex illum illum et senserit fore ea cernantur cohaerescant se aliquip eu 
-      iudicem ea enim arbitror instituendarum, est quae quem eram nescius, iis 
-      deserunt exercitation, ubi quid veniam labore nescius ita noster cernantur ita 
-      illum dolor.
-    </p>
     {
       data.allMarkdownRemark.edges
       .map(({ node: { frontmatter, fields: { slug }} }, i) => (
