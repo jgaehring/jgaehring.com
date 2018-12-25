77b40bd1f5feb4779472813bdc3657db2ef4433f
Jamie Gaehring <gaehj457@gmail.com>
2018-12-25 10:30:15 -0500

    add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,26 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131287385-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
     'gatsby-plugin-remove-trailing-slashes',
     {
       resolve: 'gatsby-plugin-manifest',
